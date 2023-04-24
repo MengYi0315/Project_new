e7e9b27d1cf81aefc86acfbba418884285cac244
AngelaChuu <s1410931005@gmail.com>
2023-04-24 21:07:50 +0800

    post js

--- 小專/首頁/js/Index_Login.js
+++ 小專/首頁/js/Index_Login.js
@@ -1,43 +1,21 @@
-function getData(url, data, headers) {
-    const queryString = `?activity_title=${data.title}&activity_content=${data.content}&FormImage=${data.image}`;
+const apiURL = 'http://localhost:5229/api/Announcement/GetAllDataList';
 
-    return fetch(`${url}${queryString}`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: headers
-    })
-      .then(response => response.json()) //輸出成json
+const getPosts = async () => {
+    try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    // 遍歷每個物件，並輸出對應的ID、標題、時間
+    data.forEach(post => {
+        console.log(`ID: ${post.id}`);
+        console.log(`標題: ${post.title}`);
+        console.log(`時間: ${post.created_at}`);
+    });
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-
-function resultvalue(result){
-    if(result==0)
-        return '0';
-    else if(result==1)
-        return '1';
-}
-
-
-function submit() {
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    const image = document.getElementById('image').files[0];
-    const token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTIzIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIkp1bmlvciIsIlNlbmlvciJdLCJleHAiOjE2ODIyMzkxMTJ9.lAlXv_IqkEq0UCtBqMuN9JF8BUS_fHZ6mSmd4ukTlz8';
-
-    const data = {
-        title: title,
-        content: content,
-        image: image,
-    };
-
-    const headers = {
-        'Authorization': `Bearer ${token}`
-    };
-
-    getData('http://localhost:5229/api/Announcement/GetAllDataList', data, headers)
-        .then(data => {
-        const result = data.result;
-        console.log(data);
-        console.log(result);
-        });
-}
+// 當頁面載入完成後自動執行getPosts函式
+window.onload = getPosts;
