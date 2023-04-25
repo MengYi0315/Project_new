7ed41f249bd1f4cc562daa9b141e208c2eaecbce
MengYi0315 <mindy.1030529@gmail.com>
2023-04-25 18:58:03 +0800

    123

---
+++ 小專/專題/js/test.js
@@ -0,0 +1,28 @@
+let navhover1 = document.getElementById('nav-a-hover-1');
+    let downbar1 = document.getElementById('down-bar-1');
+    
+
+    
+    navhover1.addEventListener('mouseover', function () {
+    downbar1.style.display = 'block';
+    })
+
+    
+    navhover1.addEventListener('mouseout', function () {
+        downbar1.style.display = 'none';
+    })
+
+    
+    let navhover2 = document.getElementById('nav-a-hover-2');
+    let downbar2 = document.getElementById('down-bar-2');
+    
+
+    
+    navhover2.addEventListener('mouseover', function () {
+    downbar2.style.display = 'block';
+    })
+
+    
+    navhover2.addEventListener('mouseout', function () {
+        downbar2.style.display = 'none';
+    })
