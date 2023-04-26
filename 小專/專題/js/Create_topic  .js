02e3743ee4b0f667226dc5727e6e7433894fe3eb
AngelaChuu <s1410931005@gmail.com>
2023-04-26 16:43:49 +0800

    js

--- 小專/專題/js/Create_topic  .js
+++ 小專/專題/js/Create_topic  .js
@@ -1,9 +1,8 @@
 function postData(url, data, headers) {
     const formData = new FormData();
-    formData.append('seniorproject_title', data.title);
-    formData.append('seniorproject_content', data.content);
+    formData.append('activity_title', data.title);
+    formData.append('activity_content', data.content);
     formData.append('FormImage', data.image);
-    formData.append('seniorproject_year', data.year);
 
     return fetch(url, {
         method: 'POST',
@@ -14,18 +13,23 @@ function postData(url, data, headers) {
       .then(response => response.formData) //輸出成json
 }
 
+
+function resultvalue(result){
+    if(result==0)
+        return '0';
+    else if(result==1)
+        return '1';
+}
+
+
 function submit() {
-    const senior_title = document.getElementById('title').value;
-    const senior_year = document.getElementById('year').value;
-    const senior_person = document.getElementById('person').value;
-    const senior_image = document.getElementById('image').files[0];
-    const senior_content = document.getElementById('content').value;
-    const token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTIzIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIkp1bmlvciIsIlNlbmlvciJdLCJleHAiOjE2ODIyMzkxMTJ9.lAlXv_IqkEq0UCtBqMuN9JF8BUS_fHZ6mSmd4ukTlz8';
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+    const image = document.getElementById('image').files[0];
+    const token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMzIxIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIkp1bmlvciIsIlNlbmlvciJdLCJleHAiOjE2ODIzNDgyOTV9.VNBrdxtbhMjA1repGA351lAt09Qo9quUcUizy7-JfeA';
 
     const data = {
         title: title,
-        year: year,
-        person:person,
         content: content,
         image: image,
     };
@@ -45,7 +49,7 @@ function submit() {
 
 
 // function previewFile() {
-//     var preview = document.querySelector('.img');
+//     var preview = document.querySelector('#img');
 //     var file    = document.querySelector('input[type=file]').files[0];
 //     var reader  = new FileReader();
 
@@ -54,9 +58,7 @@ function submit() {
 //     }, false);
 
 //     if (file) {
-//         reader.readAsDataURL(file);
-//         //設定圖片大小
-//         preview.style.width = "300px";
+//     reader.readAsDataURL(file);
 //     }
 // }
 
@@ -67,6 +69,10 @@ function previewFile() {
     var file    = document.querySelector('input[type=file]').files[0];
     var reader  = new FileReader();
 
+    // 取得檔案名稱
+    var fileName = file.name;
+    var fileNameDisplay = document.querySelector('.file-name');
+    fileNameDisplay.textContent = fileName;
 
     reader.addEventListener("load", function () {
         preview.src = reader.result;
---
+++ 小專/歷屆得獎紀錄/js/Create_Award.js
@@ -0,0 +1,34 @@
+function postData(url,data,headers){
+
+    return fetch(url, {
+        body: JSON.stringify(data),
+        headers:headers,
+        method:'POST',
+        mode:'cors',
+    })
+    //.then(response => response.json()) //輸出成json
+}
+
+function submit(){
+    const contest_year=document.getElementById('year').value;
+    const contest_name=document.getElementById('name').value;
+    const contest_work=document.getElementById('work').value;
+    const contest_rank=document.getElementById('rank').value;
+    const token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTIzIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIkp1bmlvciIsIlNlbmlvciJdLCJleHAiOjE2ODI0OTkyODR9.79RWImknKFgbKtd0Azcu0nhf922RGMie37ALaQbclX4';
+
+    const data={
+        contest_year,
+        contest_name,
+        contest_work,
+        contest_rank
+    }
+    const headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+    };
+    postData('http://localhost:5229/api/ContestAward/CreateData',data,headers)
+    .then(data=>{
+        // sessionStorage.setItem('LoginData', JSON.stringify(data));
+        console.log(data);
+    })
+}
