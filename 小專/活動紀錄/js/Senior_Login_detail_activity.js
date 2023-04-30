a1b2a989a428d58e23c14e1bd97f8ef367eac74e
AngelaChuu <s1410931005@gmail.com>
2023-05-01 01:36:07 +0800

    activity link

---
+++ 小專/活動紀錄/js/Senior_Login_detail_activity.js
@@ -0,0 +1,102 @@
+
+
+function readone(id) {
+    
+    var popup = document.querySelector("#popup");
+    console.log('Readone data:', `${id}`); // 调试信息
+    
+    fetch(`http://localhost:5229/api/Activity/ReadOneData?id=${id}`)
+    .then(response => response.json())
+    .then(data => {
+        popup.innerHTML = "";
+        popup.innerHTML +=
+        `
+            <div class="absolute-x "><a href="./Senior-Login_activity.html">X</a></div>
+            <div class="reveal-left flex">
+                <img src="${data.first_image}" class="b-25">
+            </div>
+            <div class="reveal-right flex">
+                <div class="title-detail">${data.activity_title}</div>
+                <div class="activity-detail">${data.activity_content}</div>
+            </div>
+            
+            
+        `;
+        console.log(data);
+    })
+    .catch(error => console.error(error));
+}
+function deleteData(url, id) {
+    console.log('Deleting data:', `${url}${id}`); // 调试信息
+    return fetch(`${url}${id}`, {
+        method: 'DELETE',
+        headers: {'Authorization': `Bearer ${LoginToken}`}
+    })
+    .then(data => {
+        console.log(data); // 刪除成功後的回應
+        fetch("http://localhost:5229/api/Activity/GetAllDataList")
+        .then(response => response.json())
+        .then(data => {
+            post.innerHTML = "";
+            data.forEach((item) => {
+                post.innerHTML +=
+                `
+                <div class="relative w-25 flex">
+                    <a href="./Senior-Login_detail_activity.html?id=${item.activity_id}" class="a1">
+                        <img src="${item.first_image}" class="b-25">
+                        <div class="center">${item.activity_title}</div>
+                    </a>
+                </div>
+                `;
+            });
+            window.onload();
+        })
+        .catch(error => console.error(error));
+    })
+    .catch(error => {
+        console.error('There was a problem deleting data:', error);
+    });
+}
+
+window.onload = function (){
+    
+    const url = window.location.href;
+    console.log("url",url);
+    var split = url.split("=");
+    var id = split[1];
+    console.log(id);
+    readone(id);
+}
+// window.onload = function() {
+//     var post = document.querySelector("#post");
+//     fetch("http://localhost:5229/api/Activity/GetAllDataList")
+//     .then(response => response.json())
+//     .then(data => {
+//         post.innerHTML = "";
+//         data.forEach((item) => {
+//             post.innerHTML +=
+//             `
+//             <div class="relative w-25 flex">
+//                 <a href="./Admin_Login_detail_activity.html?id=${item.activity_id}" class="a1">
+//                     <img src="${item.first_image}" class="b-25">
+//                     <div class="center">${item.activity_title}</div>
+//                 </a><div class="absolute"><input class="submit" value="X" type="submit" onclick="deleteData('http://localhost:5229/api/Activity/DeleteData?id=', '${item.activity_id}')"></div>
+//             </div>
+//             `;
+//         });
+//     })
+//     .catch(error => console.error(error));
+// };
+        // popup.innerHTML +=
+        //     `
+        //         <div class="absolute-x "><a href="http://127.0.0.1:5555/%E5%B0%8F%E5%B0%88/%E6%B4%BB%E5%8B%95%E7%B4%80%E9%8C%84/Admin_Login_activity.html">X</a></div>
+        //         <div class="reveal-left flex">
+        //             <a href="http://127.0.0.1:5555/%E5%B0%8F%E5%B0%88/%E6%B4%BB%E5%8B%95%E7%B4%80%E9%8C%84/Admin_Login_detail_activity.html?id=${item.activity_id}"><img src="${item.first_image}" class="b-25"></a>
+        //         </div>
+        //         <div class="reveal-right flex">
+        //             <div class="title-detail">${item.activity_title}</div>
+        //             <div class="activity-detail">${item.activity_content}</div>
+        //         </div>
+        //         <div class="absolute-edit"><a href="./Edit_Login_activity?id=${item.activity_id}"><img src="./img/edit.PNG" style="width: 45px;height:40px;"></a></div>
+                
+        //     `;
