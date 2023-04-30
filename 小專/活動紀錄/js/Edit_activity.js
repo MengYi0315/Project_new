5ae922bf9355f4e25ed864847763113df635b5ca
AngelaChuu <s1410931005@gmail.com>
2023-04-30 15:17:44 +0800

    edit js

--- 小專/活動紀錄/js/Edit_activity.js
+++ 小專/活動紀錄/js/Edit_activity.js
@@ -1,27 +1,6 @@
 let LoginToken=sessionStorage.getItem('LoginToken');
-function submit() {
-    console.log(LoginToken);
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    const image = document.getElementById('image').files[0];
-
-    const data = {
-        title: title,
-        content: content,
-        image: image,
-    };
-
-    const headers = {
-        'Authorization': `Bearer ${LoginToken}`
-    };
-    
-
-    postData('http://localhost:5229/api/Activity/CreateData', data, headers)
-        .then(({data}) => {
-            console.log(data);
-            
-        });
-}
+console.log(LoginToken);
+let announcementData;
 
 function previewFile() {
     var preview = document.querySelector('.img');
@@ -56,9 +35,9 @@ function submit(id) {
         }
     })
     
-    
     .then(response => response.json())
     .then(data => {
+        announcementData = data;
         post.innerHTML = "";
         post.innerHTML +=
         `
@@ -71,7 +50,7 @@ function submit(id) {
             <p class="file-name" style="font-size:20px;font-weight: bold;"></p>
         </div>
         
-        <a href="./Admin_Login_activity.html"><input type="submit" onclick="update(${id})" value="修改" class="create-button"></a>
+        <input type="submit" onclick="update('${id}')" value="修改" class="create-button">
         <a href="./Admin_Login_activity.html"><input type="button" value="返回" class="create-button"></a>
             
         `;
@@ -79,48 +58,32 @@ function submit(id) {
     }) 
 }
 
-function update(id)
-{
-    var post = document.querySelector("#post");
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    const image = document.getElementById('image').files[0];
+function update(id) { 
+    const titleInput = document.querySelector('input[type="text"]').value;
+    const contentInput = document.querySelector('textarea').value;
+    const image = document.querySelector('input[type="file"]');
+
+    // 创建一个 FormData 对象，并添加要修改的图片和文字内容
+    
     const formData = new FormData();
-    formData.append('activity_title', title);
-    formData.append('activity_content', content);
-    formData.append('FormImage', image);
+    formData.append('activity_title', titleInput);
+    formData.append('activity_content', contentInput);
+    formData.append('FormImage', image.files[0]);
+
     fetch(`http://localhost:5229/api/Activity/UpdateData?id=${id}`, {
         method: 'PUT',
         mode: 'cors',
         headers: {
-            'Content-Type': 'application/json',
             'Authorization': `Bearer ${LoginToken}`
         },
-        body:formData,
+        body: formData
     })
-    
-    
-    .then(response => response.json())
+
     .then(data => {
-        post.innerHTML = "";
-        post.innerHTML +=
-        `
-        <div class="title">修改活動</div>
-        <div class="create-text">名稱：<input type="text" id="title"class="create-input" value="${activity_title}"></div>
-        <div class="create-text flex"><div class="flex"style="justify-content: flex-start;">描述：</div><textarea id="content" style="font-size: 20px;">${activity_content}</textarea></div>
-        <div class="create-text">圖片：<input type="file" id="image" onchange="previewFile()" style="justify-content: right;"class="create-input"></div>
-        <div style="margin-left:10px; margin-bottom:20px;">
-            <img class="img" src="${data.first_image}" style="width:300px;">
-            <p class="file-name" style="font-size:20px;font-weight: bold;"></p>
-        </div>
-        
-        <input type="submit" onclick="update(${id})" value="修改" class="create-button">
-        <a href="./Admin_Login_activity.html"><input type="button" onclick="submit()" value="返回" class="create-button"></a>
-            
-        `;
         console.log(data);
     });
 }
+
 
 window.onload = function (){
     
@@ -131,3 +94,117 @@ window.onload = function (){
     console.log(id);
     submit(id);
 }
+
+
+// let LoginToken=sessionStorage.getItem('LoginToken');
+// function submit() {
+//     console.log(LoginToken);
+//     const title = document.getElementById('title').value;
+//     const content = document.getElementById('content').value;
+//     const image = document.getElementById('image').files[0];
+
+//     const data = {
+//         title: title,
+//         content: content,
+//         image: image,
+//     };
+
+//     const headers = {
+//         'Authorization': `Bearer ${LoginToken}`
+//     };
+    
+
+//     postData('http://localhost:5229/api/Activity/CreateData', data, headers)
+//         .then(({data}) => {
+//             console.log(data);
+            
+//         });
+// }
+
+
+// function submit(id) {
+//     var post = document.querySelector("#post");
+
+//     fetch(`http://localhost:5229/api/Activity/ReadOneData?id=${id}`, {
+//         method: 'GET',
+//         mode: 'cors',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+    
+    
+//     .then(response => response.json())
+//     .then(data => {
+//         post.innerHTML = "";
+//         post.innerHTML +=
+//         `
+//         <div class="title">修改活動</div>
+//         <div class="create-text">名稱：<input type="text" id="title"class="create-input" value="${data.activity_title}"></div>
+//         <div class="create-text flex"><div class="flex"style="justify-content: flex-start;">描述：</div><textarea id="content" style="font-size: 20px;">${data.activity_content}</textarea></div>
+//         <div class="create-text">圖片：<input type="file" value="${data.first_image}"id="image"onchange="previewFile()"style="justify-content: right;"class="create-input"></div>
+//         <div style="margin-left:10px;margin-bottom:20px;">
+//             <img class="img" src="${data.first_image}" style="width:300px;">
+//             <p class="file-name" style="font-size:20px;font-weight: bold;"></p>
+//         </div>
+        
+//         <a href="./Admin_Login_activity.html"><input type="submit" onclick="update(${id})" value="修改" class="create-button"></a>
+//         <a href="./Admin_Login_activity.html"><input type="button" value="返回" class="create-button"></a>
+            
+//         `;
+//         console.log(data);
+//     }) 
+// }
+
+// function update(id)
+// {
+//     var post = document.querySelector("#post");
+//     const title = document.getElementById('title').value;
+//     const content = document.getElementById('content').value;
+//     const image = document.getElementById('image').files[0];
+//     const formData = new FormData();
+//     formData.append('activity_title', title);
+//     formData.append('activity_content', content);
+//     formData.append('FormImage', image);
+//     fetch(`http://localhost:5229/api/Activity/UpdateData?id=${id}`, {
+//         method: 'PUT',
+//         mode: 'cors',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${LoginToken}`
+//         },
+//         body:formData,
+//     })
+    
+    
+//     .then(response => response.json())
+//     .then(data => {
+//         post.innerHTML = "";
+//         post.innerHTML +=
+//         `
+//         <div class="title">修改活動</div>
+//         <div class="create-text">名稱：<input type="text" id="title"class="create-input" value="${activity_title}"></div>
+//         <div class="create-text flex"><div class="flex"style="justify-content: flex-start;">描述：</div><textarea id="content" style="font-size: 20px;">${activity_content}</textarea></div>
+//         <div class="create-text">圖片：<input type="file" id="image" onchange="previewFile()" style="justify-content: right;"class="create-input"></div>
+//         <div style="margin-left:10px; margin-bottom:20px;">
+//             <img class="img" src="${data.first_image}" style="width:300px;">
+//             <p class="file-name" style="font-size:20px;font-weight: bold;"></p>
+//         </div>
+        
+//         <input type="submit" onclick="update(${id})" value="修改" class="create-button">
+//         <a href="./Admin_Login_activity.html"><input type="button" onclick="submit()" value="返回" class="create-button"></a>
+            
+//         `;
+//         console.log(data);
+//     });
+// }
+
+// window.onload = function (){
+    
+//     const url = window.location.href;
+//     console.log("url",url);
+//     var split = url.split("=");
+//     var id = split[1];
+//     console.log(id);
+//     submit(id);
+// }
--- 小專/首頁/js/Carousel.js
+++ 小專/首頁/js/Carousel.js
@@ -1,3 +1,45 @@
+
+fetch("http://localhost:5229/api/Carousel/GetAllDataList")
+  .then((response) => response.json())
+  .then((data) => {
+    const imageContainer = document.getElementById("slideshow");
+    const dotsContainer = document.getElementById("circle");
+
+    data.map((item) => {
+        return {
+            id: item.carousel_image_id,
+            url: item.carousel_image,
+        };
+        })
+        .forEach((item,index) => {
+            const div = document.createElement("div");
+            div.className = 'mySlides fade';
+            div.id = `slide-${index + 1}`;
+            const img = document.createElement("img");
+            img.src = item.url;
+            img.id = `${item.id}`;
+            img.className = 'carousel-set';
+            div.appendChild(img);
+            imageContainer.appendChild(div);
+
+            const dot = document.createElement("span");
+            dot.className = 'dot';
+            dot.onclick = function() {
+                currentSlide(index + 1);
+            };
+            dotsContainer.appendChild(dot);
+        });
+        
+        console.log(imageContainer);
+
+        showSlides(1);
+    })
+    .catch((error) => console.error(error));
+
+setInterval(function() {
+    plusSlides(1);
+    }, 5000);
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,4 +68,35 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) { slideIndex = 1 }
+//     if (n < 1) { slideIndex = slides.length }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+// }
 
