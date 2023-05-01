18e3f9522dfac8ad76945a21fbd30d4f96a4e440
AngelaChuu <s1410931005@gmail.com>
2023-05-01 17:52:03 +0800

    123

---
+++ 小專/歷屆得獎紀錄/js/Create_Index.js
@@ -0,0 +1,159 @@
+let LoginToken=sessionStorage.getItem('LoginToken');
+console.log(LoginToken);
+function postData(url, data, headers) {
+    return fetch(url, {
+        body: JSON.stringify(data),
+        headers: headers,
+        method: 'POST',
+        mode: 'cors',
+    })
+    //.then(response => response.json()) //輸出成json
+}
+
+function submit() {
+    console.log(LoginToken);
+    const announce_title = document.getElementById('title').value;
+    const announce_content = document.getElementById('content').value;
+
+
+    const data = {
+        announce_title,
+        announce_content
+    }
+    const headers = {
+        'Authorization': `Bearer ${LoginToken}`,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    };
+    postData('http://localhost:5229/api/Announcement/CreateData', data, headers)
+        .then(({data}) => {
+            console.log(data);
+            window.onload();
+            window.alert("新增成功");
+        })
+        .catch(error => {
+            console.error(error);
+            window.alert("新增失敗");
+        })
+}
+
+
+function deleteData(url, id) {
+    console.log('Deleting data:', `${url}${id}`); // 调试信息
+    return fetch(`${url}${id}`, {
+        method: 'DELETE',
+        headers: {'Authorization': `Bearer ${sessionStorage.getItem('LoginToken')}`}
+    })
+    .then(data => {
+        console.log(data); // 刪除成功後的回應
+        window.onload();
+        window.alert("刪除成功");
+    })
+    .catch(error => {
+        console.error('There was a problem deleting data:', error);
+        window.alert("刪除失敗ˋ");
+    });
+}
+
+window.onload = function() {
+    var post = document.querySelector("#post");
+    fetch("http://localhost:5229/api/Announcement/GetAllDataList")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        data.sort((a, b) => new Date(b.update_time) - new Date(a.update_time));
+        post.innerHTML = "";
+        data.forEach((item) => {
+            const date = new Date(item.update_time);
+            const update_time = date.toLocaleString();
+
+            post.innerHTML +=
+            `
+            <div class="flex">
+                <div style="width:100%;">
+                    <a href="/小專/首頁/Admin_Login_Index_detail.html" class="a1 flex">
+                        <div class="space-between flex">
+                            <div> 標題： ${item.announce_title}</div>
+                            <div> 更新時間： ${update_time}</div>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="flex">
+                    <a href="./Edit_Login_Index.html?id=${item.announce_id}"  style="color:#FFF;"><input type="submit" class="edit-row-button" value="修改"></a>
+                    <input type="submit" onclick="deleteData('http://localhost:5229/api/Announcement/DeleteData?id=', '${item.announce_id}')" class="delete-row-button" value="刪除">
+                </div>
+            </div>
+            `;
+        });
+    })
+}
+
+
+// function postData(url,data,headers){
+
+//     return fetch(url, {
+//         body: JSON.stringify(data),
+//         headers:headers,
+//         method:'POST',
+//         mode:'cors',
+//     })
+//     //.then(response => response.json()) //輸出成json
+// }
+
+// function submit(){
+//     const announce_title=document.getElementById('title').value;
+//     const announce_content=document.getElementById('content').value;
+//     const token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTIzIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIkp1bmlvciIsIlNlbmlvciJdLCJleHAiOjE2ODI1MDM3MzV9.dU4k9OBupPvholFaKwWM_RJaedcrItBZ3NnwR6V21Fw';
+
+//     const data={
+//         announce_title,
+//         announce_content
+//     }
+//     const headers = {
+//         'Authorization': `Bearer ${token}`,
+//         'Content-Type': 'application/json'
+//     };
+//     postData('http://localhost:5229/api/Announcement/CreateData',data,headers)
+//     .then(data=>{
+//         // sessionStorage.setItem('LoginData', JSON.stringify(data));
+//         console.log(data);
+//     })
+// }  
+
+
+// function getToken() {
+//     const loginData = JSON.parse(sessionStorage.getItem('LoginData'));
+//     return loginData.token;
+// }
+
+// function postData(url,data,headers){
+
+//     return fetch(url, {
+//         body: JSON.stringify(data),
+//         headers:headers,
+//         method:'POST',
+//         mode:'cors',
+//     })
+//     //.then(response => response.json()) //輸出成json
+// }
+
+// function submit(){
+//     const announce_title=document.getElementById('title').value;
+//     const announce_content=document.getElementById('content').value;
+//     const token = getToken();
+
+//     const data={
+//         announce_title,
+//         announce_content
+//     }
+//     const headers = {
+//         'Authorization': `Bearer ${token}`,
+//         'Content-Type': 'application/json'
+//     };
+//     postData('http://localhost:5229/api/Announcement/CreateData',data,headers)
+//     .then(data=>{
+//         sessionStorage.setItem('LoginData', JSON.stringify(data));
+//         console.log(data);
+//     })
+// }
---
+++ 小專/歷屆得獎紀錄/js/Edit_Index.js
@@ -0,0 +1,231 @@
+let LoginToken=sessionStorage.getItem('LoginToken');
+console.log(LoginToken);
+
+function updateData(id) {
+    var post = document.querySelector("#post");
+    // 讀取舊有資料
+    fetch(`http://localhost:5229/api/Announcement/ReadOneData?id=${id}`, {
+    headers: {
+        'Authorization': `Bearer ${LoginToken}`
+    }
+    })
+    .then(response => response.json())
+    .then(data => {
+
+        post.innerHTML = "";
+
+        post.innerHTML +=
+        `
+        <div class="title">修改公告</div>
+        <div class="create-content">標題：<input style="line-height: 25px;width:500px;margin: 20px 0px;" type="text" value="${data.announce_title}"></div>
+        <div class="create-content flex">
+            內容：<textarea name="" id="" cols="74" rows="9">${data.announce_content}</textarea>
+            
+        </div>
+        <div style="margin-left:20px;">
+            <input type="button" value="發布" class="submit">
+        </div> 
+        `;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// window.onload = function (){
+    
+//     const url = window.location.href;
+//     console.log("url",url);
+//     var split = url.split("=");
+//     var href = split[0];
+//     var id = split[1];
+//     updateData(id);
+// }
+
+function submit() {
+    console.log(LoginToken);
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+
+    const data = {
+        title: title,
+        content: content,
+    };
+
+    const headers = {
+        'Authorization': `Bearer ${LoginToken}`
+    };
+    
+
+    postData('http://localhost:5229/api/Activity/CreateData', data, headers)
+        .then(({data}) => {
+            console.log(data);
+            
+            window.alert("更新成功");
+        });
+}
+
+function previewFile() {
+    var preview = document.querySelector('.img');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+
+    // 取得檔案名稱
+    var fileName = file.name;
+    var fileNameDisplay = document.querySelector('.file-name');
+    fileNameDisplay.textContent = fileName;
+
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+
+    // 設定圖片大小
+    preview.style.width = "300px";
+}
+
+function submit(id) {
+    var post = document.querySelector("#post");
+
+    fetch(`http://localhost:5229/api/Announcement/ReadOneData?id=${encodeURIComponent(id)}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    
+    .then(response => response.json())
+    .then(data => {
+        announcementData = data;
+        post.innerHTML = "";
+        post.innerHTML +=
+        `
+        <div class="title">修改公告</div>
+        <div class="create-content">標題：<input style="line-height: 25px;width:500px;margin: 20px 0px;" type="text" value="${data.announce_title}"></div>
+        <div class="create-content flex">
+            內容：<textarea name="" id="" cols="74" rows="9">${data.announce_content}</textarea>
+            
+        </div>
+        <div style="margin-left:20px;">
+            <input type="submit" onclick="update('${id}')" value="發布" class="submit">
+        </div> 
+            
+        `;
+        console.log(data);
+    }) 
+}
+
+function update(id)
+{
+    const titleInput = document.querySelector('input[type="text"]');
+    const contentInput = document.querySelector('textarea');
+
+    const data = {
+        announce_title: titleInput.value.toString(),
+        announce_content: contentInput.value.toString()
+    };
+    fetch(`http://localhost:5229/api/Announcement/UpdateData?id=${encodeURIComponent(id)}`, {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${LoginToken}`
+        },
+        body: JSON.stringify(data) // 加入要傳送的公告內容
+    })
+    .then(data => {
+        console.log(data);
+    });
+}
+
+window.onload = function (){
+    
+    const url = window.location.href;
+    console.log("url",url);
+    var split = url.split("=");
+    var id = split[1];
+    console.log(id);
+    submit(id);
+}
+
+
+
+// post.innerHTML = "";
+// post.innerHTML +=
+// `
+// <div class="title">修改公告</div>
+// <div class="create-content">標題：<input style="line-height: 25px;width:500px;margin: 20px 0px;" type="text" value="${data.announce_title}"></div>
+// <div class="create-content flex">
+//     內容：<textarea name="" id="" cols="74" rows="9">${data.announce_content}</textarea>
+    
+// </div>
+// <div style="margin-left:20px;">
+//     <input type="submit" onclick="update(id)" value="發布" class="submit" >
+// </div> 
+    
+// `;
+// window.location.href="http://127.0.0.1:5555/小專/首頁/Admin_Login_Index.html";
+
+
+// function submit() {
+//     console.log(LoginToken);
+//     const title = document.getElementById('title').value;
+//     const content = document.getElementById('content').value;
+
+//     const data = {
+//         title: title,
+//         content: content,
+//     };
+
+//     const headers = {
+//         'Authorization': `Bearer ${LoginToken}`
+//     };
+    
+
+//     postData('http://localhost:5229/api/Activity/CreateData', data, headers)
+//         .then(({data}) => {
+//             console.log(data);
+            
+//         });
+// }
+
+// function updateData(id) {
+//     var post = document.querySelector("#post");
+//     // 讀取舊有資料
+//     fetch(`http://localhost:5229/api/Announcement/ReadOneData?id=${id}`, {
+//     headers: {
+//         'Authorization': `Bearer ${LoginToken}`
+//     }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+
+//         post.innerHTML = "";
+
+//         post.innerHTML +=
+//         `
+//         <div class="title">修改公告</div>
+//         <div class="create-content">標題：<input style="line-height: 25px;width:500px;margin: 20px 0px;" type="text" value="${data.announce_title}"></div>
+//         <div class="create-content flex">
+//             內容：<textarea name="" id="" cols="74" rows="9">${data.announce_content}</textarea>
+            
+//         </div>
+//         <div style="margin-left:20px;">
+//             <input type="button" value="發布" class="submit">
+//         </div> 
+//         `;
+//     })
+//     .catch(error => console.error('Error:', error));
+// }
+
+// window.onload = function (){
+    
+//     const url = window.location.href;
+//     console.log("url",url);
+//     var split = url.split("=");
+//     var href = split[0];
+//     var id = split[1];
+//     updateData(id);
+// }
