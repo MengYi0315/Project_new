58bfdc75446d086db15156c7b4fc4858ddf0260f
AngelaChuu <s1410931005@gmail.com>
2023-05-02 15:27:03 +0800

    123

---
+++ 小專/歷屆得獎紀錄/js/Create_Award.js
@@ -0,0 +1,38 @@
+let LoginToken=sessionStorage.getItem('LoginToken');
+console.log(LoginToken);
+function postData(url, data, headers) {
+    return fetch(url, {
+        body: JSON.stringify(data),
+        headers: headers,
+        method: 'POST',
+        mode: 'cors',
+    })
+    //.then(response => response.json()) //輸出成json
+}
+
+function submit() {
+    console.log(LoginToken);
+    const contest_year = document.getElementById('contest_year').value;
+    const contest_name = document.getElementById('contest_name').value;
+    const contest_work = document.getElementById('contest_work').value;
+    const contest_rank = document.getElementById('contest_rank').value;
+
+
+    const data = {
+        contest_year,
+        contest_name,
+        contest_work,
+        contest_rank
+    }
+    const headers = {
+        'Authorization': `Bearer ${LoginToken}`,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    };
+    postData('http://localhost:5229/api/ContestAward/CreateData', data, headers)
+        .then(({data}) => {
+            console.log(data);
+
+        })
+
+}
