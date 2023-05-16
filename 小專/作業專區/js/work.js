65559774d1dad27b24801cb2fc3097fde0b22fae
AngelaChuu <s1410931005@gmail.com>
2023-05-17 02:11:41 +0800

    homework new

---
+++ 小專/作業專區/js/work.js
@@ -0,0 +1,12 @@
+function ToggleMenu(number){
+    debugger
+    let nav = document.getElementById(`menu-${number}`);
+    nav.style.display = nav.style.display == "none" ? "block" : "none";
+}
+
+function ToggleNav(number){
+    let nav = document.getElementById(`nav${number}`);
+    nav.style.display = nav.style.display == "none" ? "block" : "none";
+
+}
+
