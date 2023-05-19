52d3dda135ca12801af4786eb1cd841272e61782
MengYi0315 <mindy.1030529@gmail.com>
2023-05-19 23:29:25 +0800

    rwd header/test

--- 小專/學習專區-考試列表/js/Test.js
+++ 小專/學習專區-考試列表/js/Test.js
@@ -1,6 +1,6 @@
 function ToggleMenu(number){
     let menu = document.getElementById("menu-"+number);
-    if(menu.style.display == "none"){
+    if(menu.style.display == "none" || menu.style.display == ""){
         menu.style.display = "block";
     }else{
         menu.style.display = "none";
@@ -9,7 +9,7 @@ function ToggleMenu(number){
 
 function ToggleNav(number){
     let nav = document.getElementById("nav-"+number);
-    if(nav.style.display == "none"){
+    if(nav.style.display == "none" || nav.style.display == ""){
         nav.style.display = "block";
     }else{
         nav.style.display = "none";
@@ -18,9 +18,11 @@ function ToggleNav(number){
 }
 
 
+
+
     let navhover = document.getElementById('nav-a-hover');
     let downbar = document.getElementById('down-bar');
-    let t = document.getElementById('t');
+
 
     navhover.addEventListener('mouseover', function () {
     downbar.style.display = 'block';
@@ -56,3 +58,83 @@ function ToggleNav(number){
     navhover3.addEventListener('mouseout', function () {
     downbar3.style.display = 'none';
     })
+
+
+
+    let navbox = document.getElementById('nav-box');
+    let ull = document.getElementById('ull');
+
+    navbox.onclick = function(){
+        if(ull.style.display == 'none' || ull.style.display == ''){
+            ull.style.display = 'block';
+            // navbox.style.borderBottom = '1px solid #fff'
+        }
+        else{
+            ull.style.display = 'none';
+        }nav-lnav-l
+    }
+
+    let achievement = document.getElementById('achievement');
+    let achievement_list = document.getElementById('achievement-list');
+
+    achievement.onclick = function(){
+        if(achievement_list.style.display == 'none' || achievement_list.style.display == ''){
+            achievement_list.style.display = 'block';
+        }
+        else{
+            achievement_list.style.display = 'none';
+        }
+    }
+
+
+    let study = document.getElementById('study');
+    let study_list = document.getElementById('study-list');
+
+    study.onclick = function(){
+        if(study_list.style.display == 'none' || study_list.style.display == ''){
+            study_list.style.display = 'block';
+        }
+        else{
+            study_list.style.display = 'none';
+        }
+    }
+
+
+
+
+    let person = document.getElementById('person');
+    let person_list = document.getElementById('person-list');
+
+    person.onclick = function(){
+        if(person_list.style.display == 'none' || person_list.style.display == ''){
+            person_list.style.display = 'block';
+        }
+        else{
+            person_list.style.display = 'none';
+        }
+    }
+
+    let testt = document.getElementById('testt');
+    let testt_list = document.getElementById('testt-list');
+
+    testt.onclick = function(){
+        if(testt_list.style.display == 'none' || testt_list.style.display == ''){
+            testt_list.style.display = 'block';
+        }
+        else{
+            testt_list.style.display = 'none';
+        }
+    }
+
+
+    let hw = document.getElementById('hw');
+    let hw_list = document.getElementById('hw-list');
+
+    hw.onclick = function(){
+        if(hw_list.style.display == 'none' || hw_list.style.display == ''){
+            hw_list.style.display = 'block';
+        }
+        else{
+            hw_list.style.display = 'none';
+        }
+    }
