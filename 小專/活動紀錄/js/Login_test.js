c5ec178a7f1da7253322eae83ce40f494f058c42
AngelaChuu <s1410931005@gmail.com>
2023-05-20 22:07:29 +0800

    activity header

---
+++ 小專/活動紀錄/js/Login_test.js
@@ -0,0 +1,60 @@
+let navhover = document.getElementById('nav-a-hover');
+    let downbar = document.getElementById('down-bar');
+    
+
+    
+    navhover.addEventListener('mouseover', function () {
+      downbar.style.display = 'block';
+    })
+
+    
+    navhover.addEventListener('mouseout', function () {
+        downbar.style.display = 'none';
+    })
+
+
+
+
+
+    let navhover1 = document.getElementById('nav-a-hover-1');
+    let downbar1 = document.getElementById('down-bar-1');
+    
+
+    
+    navhover1.addEventListener('mouseover', function () {
+      downbar1.style.display = 'block';
+    })
+
+    
+    navhover1.addEventListener('mouseout', function () {
+        downbar1.style.display = 'none';
+    })
+
+
+    // let navhover2 = document.getElementById('nav-a-hover-2');
+    // let downbar2 = document.getElementById('down-bar-2');
+    
+
+    
+    // navhover2.addEventListener('mouseover', function () {
+    //   downbar2.style.display = 'block';
+    // })
+
+    
+    // navhover2.addEventListener('mouseout', function () {
+    //     downbar2.style.display = 'none';
+    // })
+
+    let navhover3 = document.getElementById('nav-a-hover-3');
+    let downbar3 = document.getElementById('down-bar-3');
+    
+
+    
+    navhover3.addEventListener('mouseover', function () {
+      downbar3.style.display = 'block';
+    })
+
+    
+    navhover3.addEventListener('mouseout', function () {
+        downbar3.style.display = 'none';
+    })
