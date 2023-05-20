ee5862395aab08595f8e0a6a35bd8e42ed0d3e5d
AngelaChuu <s1410931005@gmail.com>
2023-05-20 21:52:43 +0800

    index header 2

--- 小專/活動紀錄/js/test.js
+++ 小專/活動紀錄/js/test.js
@@ -31,16 +31,30 @@ let navhover = document.getElementById('nav-a-hover');
     })
 
 
-    // let navhover2 = document.getElementById('nav-a-hover-2');
-    // let downbar2 = document.getElementById('down-bar-2');
+    let navhover2 = document.getElementById('nav-a-hover-2');
+    let downbar2 = document.getElementById('down-bar-2');
     
 
     
-    // navhover2.addEventListener('mouseover', function () {
-    //   downbar2.style.display = 'block';
-    // })
+    navhover2.addEventListener('mouseover', function () {
+      downbar2.style.display = 'block';
+    })
 
     
-    // navhover2.addEventListener('mouseout', function () {
-    //     downbar2.style.display = 'none';
-    // })
+    navhover2.addEventListener('mouseout', function () {
+        downbar2.style.display = 'none';
+    })
+
+    let navhover3 = document.getElementById('nav-a-hover-3');
+    let downbar3 = document.getElementById('down-bar-3');
+    
+
+    
+    navhover3.addEventListener('mouseover', function () {
+      downbar3.style.display = 'block';
+    })
+
+    
+    navhover3.addEventListener('mouseout', function () {
+        downbar3.style.display = 'none';
+    })
