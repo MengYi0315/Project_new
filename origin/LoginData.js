71b1f68089bc7707e6fbba1cf835b0a17ed9e22e
AngelaChuu <s1410931005@gmail.com>
2023-05-24 19:53:15 +0800

    login is_delete

--- origin/LoginData.js
+++ origin/LoginData.js
@@ -22,7 +22,7 @@ function LoginData(){
                 console.log("data",data)
                 sessionStorage.setItem('LoginToken',data.token);
                 sessionStorage.setItem('name',data.name);
-                window.alert("登入成功");
+                // window.alert("登入成功");
                 console.log(data);
                 // window.location.href="http://127.0.0.1:5500/小專/首頁/Admin_Login_Index.html";
                 if(data.is_delete === false)
