653d7772c8b4b538940287f9073acf8dd8452e5e
AngelaChuu <s1410931005@gmail.com>
2023-06-06 15:37:23 +0800

    test link

--- 小專/學習專區-考試列表/js/student-test.js
+++ 小專/學習專區-考試列表/js/student-test.js
@@ -32,19 +32,19 @@ let navhover1 = document.getElementById('nav-a-hover-1');
     })
 
     
-    // let navhover2 = document.getElementById('nav-a-hover-2');
-    // let downbar2 = document.getElementById('down-bar-2');
+    let navhover2 = document.getElementById('nav-a-hover-2');
+    let downbar2 = document.getElementById('down-bar-2');
     
 
     
-    // navhover2.addEventListener('mouseover', function () {
-    // downbar2.style.display = 'block';
-    // })
+    navhover2.addEventListener('mouseover', function () {
+    downbar2.style.display = 'block';
+    })
 
     
-    // navhover2.addEventListener('mouseout', function () {
-    //     downbar2.style.display = 'none';
-    // })
+    navhover2.addEventListener('mouseout', function () {
+        downbar2.style.display = 'none';
+    })
 
 
     // let navhover3 = document.getElementById('nav-a-hover-3');
